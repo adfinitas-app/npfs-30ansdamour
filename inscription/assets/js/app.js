461fca5b43e2d42d4da3dd530bc2083f3ec1d2c9
Sebastien Bruere <sebastien.bruere@epitech.eu>
2018-03-27 12:31:55 +0200

    Newsletter's inscription

---
+++ inscription/assets/js/app.js
@@ -0,0 +1,102 @@
+
+$(window).on('load', function () {
+    $(document).foundation();
+});
+
+
+$(document).ready( function() {
+    $("#f_phone").intlTelInput({
+        utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.12/js/utils.js',
+        initialCountry: ['fr']
+    });
+});
+
+$('#f_male').click(function() {
+    $('#f_female').prop( "checked", false );
+});
+$('#f_female').click(function() {
+    $('#f_male').prop( "checked", false );
+});
+$('input').focus(function() {
+    $(this).removeClass('red-border');
+})
+
+$('#f_summit').click( function() {
+    if (validateForm()) {
+        sendData();
+    }
+});
+
+
+function validateForm() {
+    var check = true;
+
+
+    $('.civilite-container *').css('color', "white");
+    $('.error').css('display','none');
+
+
+    /* CHECK ALL */
+
+    $('input').each( function() {
+        $(this).removeClass('red-border');
+
+        if ($(this).hasClass('required')) {
+
+            if ($(this).val() === "") {
+
+                $('.error-required').show();
+                $(this).addClass('red-border');
+                check = false;
+            }
+        }
+    });
+
+    /* CHECK EMAIL */
+
+    if ($("#f_mail").val() !== "") {
+        if (!(validateEmail($("#f_mail").val()))) {
+            $("#f_mail").addClass('red-border');
+            $('.error-email').show();
+            check = false;
+        }
+    }
+
+
+    /* CHECK PHONE */
+
+    if ($("#f_phone").val() !== "") {
+        if (!$("#f_phone").intlTelInput("isValidNumber")) {
+            $("#f_phone").addClass('red-border');
+            $('.error-phone').show();
+            check = false;
+        }
+    }
+
+
+    /* CHECK ZIPCODE */
+
+    if ($("#f_zipcode").val() !== "") {
+        if ($("#f_zipcode").val() != parseInt($("#f_zipcode").val(), 10)) {
+            $("#f_zipcode").addClass('red-border');
+            $('.error-zipcode').show();
+            check = false;
+        }
+    }
+
+    /* CHECK RECAPTCHA */
+
+    var response = grecaptcha.getResponse();
+
+    if(response.length === 0) {
+        $('.error-captcha').show();
+        check = false;
+    }
+
+    return check;
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
---
+++ inscription/assets/js/data.js
@@ -0,0 +1,192 @@
+
+
+
+
+function sendData() {
+    var data = {
+        "db": {
+            "schema": "npfs_newsletter_site\n",
+            "db": {
+                "civility": getCivility(),
+                "firstname": pureField($('#f_firstname').val().toUpperCase()),
+                "lastname": pureField($('#f_lastname').val().toUpperCase()),
+                "email": pureField($('#f_mail').val()),
+                "phone": pureField(getPhone()),
+                "address1": pureField($('#f_addr_1').val()),
+                "address2": pureField($('#f_addr_2').val()),
+                "postcode": pureField($('#f_zipcode').val()),
+                "city": pureField($('#f_city').val())
+            }
+        },
+        "woopra": {
+            "host": "spa.asso.fr",			// Nom du projet dans Woopra.
+
+            /* Variables de configuration de la fiche utilisateur, préfixe : "cv_" */
+
+            "cv_civility": getCivility(),
+            "cv_firstname": pureField($('#f_firstname').val().toUpperCase()),
+            "cv_lastname": pureField($('#f_lastname').val().toUpperCase()),
+            "cv_email": pureField($('#f_mail').val()),
+            "cv_phone": pureField(getPhone()),
+            "cv_address1": pureField($('#f_addr_1').val()),
+            "cv_address2": pureField($('#f_addr_2').val()),
+            "cv_postcode": pureField($('#f_zipcode').val()),
+            "cv_city": pureField($('#f_city').val()),
+
+            /* Variables de l'événement, : préfixe : "ce_" */
+
+            "event": "newsletter_site",				// Nom de l'événement à tracker si applicable. Non préfixé.
+            "ce_phone": pureField(getPhone())
+        },
+        "mailjet": {
+            "Email": pureField($('#f_mail').val()),
+            "Properties": {
+                "civility": getCivility(),
+                "firstname": pureField($('#f_firstname').val().toUpperCase()),
+                "lastname": pureField($('#f_lastname').val().toUpperCase()),
+                "email": pureField($('#f_mail').val()),
+                "phone": pureField(getPhone()),
+                "address1": pureField($('#f_addr_1').val()),
+                "address2": pureField($('#f_addr_2').val()),
+                "postcode": pureField($('#f_zipcode').val()),
+                "city": pureField($('#f_city').val())
+            },
+            "addLists": getList(), // Noms de transmission des listes dans lesquelles ajouter le contact. Ne pas mettre les listes "Toute la base" et "Prospects" ici, le contact y est inséré par défaut (excepté dans "Prospect" si donateur).
+            "delLists": []  // Noms de transmission des listes dans lesquelles supprimer le contact.
+        }
+    };
+
+    console.log(data);
+    //makeCorsRequest(data);
+}
+
+
+
+
+/*
+ * Debut de la lib
+ */
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
+function makeCorsRequest(data) {
+    var url = 'https://adfinitas-io.herokuapp.com/api/v1/organization/31451a9e-d1d4-4901-91de-aafe2dc78733/webhook/cc6375c0-c366-4cae-95b8-273a9bd22256';
+    var body = JSON.stringify(data);
+    var xhr = createCORSRequest('POST', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(body);
+}
+
+/*
+ * Fin de la lib
+ */
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+
+function validatePhone(phone) {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+
+    return re.test(phone);
+}
+
+
+
+
+function getPhone() {
+    return $('#f_phone').intlTelInput("getNumber");
+}
+
+function getPersonnalisationCourte() {
+    return getCivilityLong() + " " + pureField($('#f_lastname').val().toUpperCase());
+}
+
+function getPersonnalisation() {
+    return getCivilityDear() + " " + getCivilityLong() + " " + pureField($('#f_lastname').val().toUpperCase());
+}
+
+function getList() {
+    var data = [];
+
+    data.push("newsletter");
+    return data;
+}
+
+function pureField(string) {
+    return (string.replace(/'/g, "%27").replace(/"/g, "&quot;"));
+}
+
+
+function getOptin() {
+    if ($('#comm').is(":checked")) {
+        return "true";
+    }
+    return "false";
+}
+
+function getSexe() {
+    if ($('#f_male').is(":checked")) {
+        return 'Homme';
+    }
+    else {
+        return 'Femme';
+    }
+}
+
+function getCivility() {
+    if ($('#f_male').is(":checked")) {
+        return 'M';
+    }
+    else {
+        return 'MME';
+    }
+}
+
+function getCivilityDear() {
+    if ($('#f_male').is(":checked")) {
+        return 'Cher';
+    }
+    else {
+        return 'Chère';
+    }
+}
+
+function getCivilityLong() {
+    if ($('#f_male').is(":checked")) {
+        return 'MONSIEUR';
+    }
+    else {
+        return 'MADAME';
+    }
+}
